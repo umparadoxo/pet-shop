22eb286076b2c12e815fc0f5dcb2ffe8fdbaf117
umparadoxo <legiaored@gmail.com>
2021-05-29 20:54:37 -0300

    Rotas
    adicionadas as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './assets/css/base/base.css'
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
+
 
 function App() {
-  return (
-  <>
 
-  </>
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/sobre'>
+            <Sobre />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   )
 }
 
