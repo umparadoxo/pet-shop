35788332e563392e90bcc7c6cb2c94f2c3eea76a
umparadoxo <legiaored@gmail.com>
2021-05-29 22:26:23 -0300

    API e 404
    Conectado com a API e rota 404

---
+++ src/paginas/Post.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import '../assets/css/post.css'
+import { useParams, useHistory } from 'react-router-dom'
+import { busca } from '../api/api'
+
+const Post = () => {
+    let history = useHistory()
+    const { id } = useParams()
+    const [post, setPost] = useState({})
+
+    useEffect(() => {
+        busca(`/posts/${id}`, setPost)
+            .catch(() => {
+                history.push('/404')
+            })
+    }, [id])
+
+    return (
+        <main className='container flex flex--centro'>
+            <article className='cartao post'>
+                <h2 className='cartao__titulo'>
+                    {post.title}
+                </h2>
+                <p className='cartao__texto'>
+                    {post.body}
+                </p>
+            </article>
+        </main>
+    )
+}
+
+export default Post
