42ea964f18db0aa58bd26c314a24873d17d238e8
umparadoxo <legiaored@gmail.com>
2021-05-29 21:17:01 -0300

    Router
    pagina 404 e navbar

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import imagem from '../assets/img/doguito.svg'
+import '../assets/css/componentes/cabecalho.css'
+
+const Cabecalho = () => {
+    return (
+        <header className='cabecalho container'>
+            <div className='menu-hamburguer'>
+                <span className='menu-hamburguer__icone'>
+                </span>
+            </div>
+            <div className='cabecalho-container'>
+                <a href='/' className='flex flex--centro'>
+                    <img className='cabecalho__logo' src={imagem} alt='Doguito' />
+                    <h1 className='cabecalho__titulo'>PetShop</h1>
+                </a>
+            </div>
+
+            <nav className='menu-cabecalho'>
+                <ul className='menu-itens'>
+                    <li>
+                        <a className='menu-item menu-item--entrar' href='#'>Entrar</a>
+                    </li>
+                    <li>
+                        <a className='menu-item' href='#'>Produtos</a>
+                    </li>
+                    <li>
+                        <a className='menu-item' href='/'>Blog</a>
+                    </li>
+                    <li>
+                        <a className='menu-item' href='/sobre'>Sobre</a>
+                    </li>
+                </ul>
+            </nav>
+            <div className='menu-cabecalho-background'></div>
+        </header >
+
+    )
+}
+
+export default Cabecalho;
